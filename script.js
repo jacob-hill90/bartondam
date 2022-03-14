3aeacab246e12ce7d04d2cb61f7802a8152d127f
Jake <75848080+jacob-hill90@users.noreply.github.com>
2022-03-14 08:41:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.getElementById('eye').onclick = function showForm(){
-    document.getElementById("form").style.display = "flex";
-}
+let clickStates = 0 
+
+document.getElementById('eye').addEventListener("click", function(){
+    clickStates++
+    if(clickStates % 2 == 1){
+        document.getElementById('form').style.display = "flex";}
+    if(clickStates % 2 == 0){
+        document.getElementById('form').style.display = "none";}
+})
