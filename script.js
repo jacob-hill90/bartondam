85f66c96a0acc74b61b837f5241021681fbde856
Jake <75848080+jacob-hill90@users.noreply.github.com>
2022-03-19 13:32:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ let clickStates = 0
 document.getElementById('eye').addEventListener("click", function(){
     clickStates++
     if(clickStates % 2 == 1){
-        document.getElementById('form').style.display = "flex";}
+        document.getElementById('form').style.display = "flex";
+    }
     if(clickStates % 2 == 0){
-        document.getElementById('form').style.display = "none";}
+        document.getElementById('form').style.display = "none"; 
+    }
+})
+
+document.getElementById('darkMode').addEventListener("click", function(){
+    clickStates++
+    if(clickStates % 2 == 1){
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.getElementById('knight').style.backgroundImage = "url(img/bknight.png)";
+    }
+    if(clickStates % 2 == 0){
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        document.getElementById('knight').style.backgroundImage = "url(img/knight.png)";
+    }
 })
