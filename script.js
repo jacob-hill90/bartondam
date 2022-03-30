aa74e9f4b9c432db0a381ffcc8024bc65983fa78
Jake <75848080+jacob-hill90@users.noreply.github.com>
2022-03-30 06:15:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,14 @@ document.getElementById('darkMode').addEventListener("click", function(){
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
         document.getElementById('knight').style.backgroundImage = "url(img/bknight.png)";
+        document.getElementById('blackText').style.display = "block";  
+        document.getElementById('whiteText').style.display = "none";
     }
     if(clickStates % 2 == 0){
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
         document.getElementById('knight').style.backgroundImage = "url(img/knight.png)";
+        document.getElementById('blackText').style.display = "none";  
+        document.getElementById('whiteText').style.display = "block";
     }
 })
